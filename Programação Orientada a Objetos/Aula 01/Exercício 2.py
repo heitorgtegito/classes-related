e48e3ac568a1b180714f1dbae80c62f8f3ccf9e2
heitorgtegito <heitor.egito@escolar.ifrn.edu.br>
2026-03-12 14:23:18 -0300

    Create Exercício 2.py

---
+++ Programação Orientada a Objetos/Aula 01/Exercício 2.py
@@ -0,0 +1,12 @@
+# Exercício 2: Escreva um programa em Python para ler o primeiro nome de uma pessoa e mostrar a mensagem:
+
+# “Bem-vindo(a) ao Codespaces, <nome>”, onde <nome> é o nome informado pela pessoa.
+
+# Exemplo:
+# Digite seu primeiro nome:
+#   Gilbert
+# Bem-vindo(a) ao Codespaces, Gilbert
+
+nome = input("Digite o seu primeiro nome\n")
+print(f"Bem-vindo(a) ao Codespace, {nome}")
+
